1d2a22e8a94534ff907a2ac237ee59f4750a4d05
高运 <1562211757@qq.com>
2018-07-23 09:20:29 +0800

    后台代码搭建。

---
+++ src/main/java/com/gy/wo/api/service/support/OrderType.java
@@ -0,0 +1,12 @@
+package com.gy.wo.api.service.support;
+
+/**
+ * @Author gy
+ * @Date: Create in 2018/4/2 14:10
+ * @Description
+ * @Modified By:
+ */
+public enum OrderType {
+    ASC,
+    DESC
+}
