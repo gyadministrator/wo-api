b878dbe39007262352d720f251b8ba349ce4ef36
高运 <1562211757@qq.com>
2018-07-23 09:09:40 +0800

    后台代码搭建。

--- src/main/java/com/gy/shop/shopapi/exception/ServerErrorException.java
+++
@@ -1,8 +0,0 @@
-package com.gy.shop.shopapi.exception;
-
-public class ServerErrorException extends RuntimeException {
-
-    public ServerErrorException(String message) {
-        super("服务器内部错误:" + message);
-    }
-}
